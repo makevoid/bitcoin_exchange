a424044bc9d4b5343259185ff8817a566ddf4805
Francesco Canessa <makevoid@gmail.com>
2014-07-03 12:12:59 +0200

    some initial orderbook tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,14 @@ path = File.expand_path "../../", __FILE__
 
 ENV["RACK_ENV"] = "test"
 require "#{path}/config/env"
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :should
+  end
+end
+
+
+# prepare test db
+
+R.flushdb
