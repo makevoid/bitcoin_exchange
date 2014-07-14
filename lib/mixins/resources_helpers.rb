528921d9999886a0991c04b1ce94a76ce45e2f46
Francesco Canessa <makevoid@gmail.com>
2014-07-14 05:42:27 +0200

    view code fixes, mainly orders

--- lib/mixins/resources_helpers.rb
+++ lib/mixins/resources_helpers.rb
@@ -1,15 +1,19 @@
 module ResourcesHelpers
   def order_partial(order)
-    haml_tag :span do
-      haml_concat "#{order.type.upcase} #{order.amount} BTC"
+    haml_tag :div, class: "row" do
+      haml_tag :span do
+        haml_concat "#{order.type.upcase} #{order.amount} BTC"
+      end
+      haml_concat "@ €#{order.price.f_eur}"
     end
-    haml_concat "@ €#{order.price.f_eur}"
   end
   
   def order_closed_partial(order)
-    haml_tag :span do
-      haml_concat "#{order.type.upcase} €#{order.price.f_eur}"
+    haml_tag :div, class: "row" do
+      haml_tag :span do
+        haml_concat "#{order.type.upcase} €#{order.price.f_eur}"
+      end
+      haml_concat "[#{order.amount} BTC] @ #{time_f order.time}"
     end
-    haml_concat "[#{order.amount} BTC] @ #{time_f order.time}"
   end
 end
